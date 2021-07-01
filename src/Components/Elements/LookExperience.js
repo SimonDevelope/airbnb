33affb392c6b79be9a9b6aa512a1b9ec4a47f3ff
SeungMin, Yoo <whwnine@gmail.com>
2021-07-02 00:35:39 +0900

    main페이지 체험둘러보기 구현 (#2)

--- src/Components/Elements/LookExperience.js
+++ src/Components/Elements/LookExperience.js
@@ -29,10 +29,127 @@ function Experience () {
                                                 </div>
                                             </div>
                                         </div>
-                                        <div className='Big-N-Small-word-total-wrapper'></div>
+                                        <div className='Big-N-Small-word-total-wrapper'>
+                                            <div className='Big-N-Small-word-inner-wrapper'>
+                                                <h1 
+                                                tabIndex='-1'
+                                                className='Big-N-Small-title-outter-wrapper'
+                                                >
+                                                    <div className='Big-N-Small-title-inner-wrapper'>
+                                                        <span className='Last-title-wrapper'>체험 둘러보기</span>
+                                                    </div>
+                                                </h1>
+                                                <div className='Big-N-Small-inner-content-word-wrapper'>
+                                                    <span className='Last-inner-content-word-wrapper'>현지 전문가와 함께하는 독특한 액티비티에 오프라인이나 온라인으로 참여해&nbsp;보세요</span>
+                                                </div>
+                                            </div>
+                                        </div>
                                     </section>
                                 </div>
-                                <div></div>
+                                <div>
+                                    <div className='Experience-total-images-view-port-wrapper'>
+                                        <div className='Experience-total-images-inner-wrapper'>
+                                            <ul className='Image-li-tag-total-wrapper'>
+                                                <li 
+                                                aria-hidden='false'
+                                                className='Experience-first-image-total-wrapper'
+                                                >
+                                                    <div className='Experience-first-image-inner-wrapper'>
+                                                        <a
+                                                        href='/'
+                                                        className='Experience-first-address-wrapper'
+                                                        >
+                                                            <div className='Experience-first-imager-inner-wrapper'>
+                                                                <picture>
+                                                                    <source
+                                                                    srcSet='https://a0.muscache.com/im/pictures/247a1ea3-946d-4eb8-a6ab-e8b9a66846f4.jpg?im_w=320'
+                                                                    ></source>
+                                                                    <img
+                                                                    className='Experience-image-attr'
+                                                                    src='https://a0.muscache.com/im/pictures/247a1ea3-946d-4eb8-a6ab-e8b9a66846f4.jpg?im_w=720'
+                                                                    alt='https://a0.muscache.com/im/pictures/247a1ea3-946d-4eb8-a6ab-e8b9a66846f4.jpg?im_w=720'
+                                                                    ></img>
+                                                                </picture>
+                                                            </div>
+                                                            <div className='Experience-first-image-explain-wrapper'>
+                                                                <div className='recomendation-title-word-outter-wrapper'>
+                                                                    <span className='First-image-title-attr'>추천 컬렉션: 여행 본능을 깨우는 체험</span>
+                                                                </div>
+                                                                <div className='recomendation-contents-word-outter-wrapper'>
+                                                                    <span className='First-image-content-attr'>온라인 체험으로 집에서 랜선 여행을 즐기세요.</span>
+                                                                </div>
+                                                            </div>
+                                                        </a>
+                                                    </div>
+                                                </li>
+                                                <li 
+                                                aria-hidden='false'
+                                                className='Experience-second-image-total-wrapper'
+                                                >
+                                                    <div className='Experience-first-image-inner-wrapper'>
+                                                        <a
+                                                        href='/'
+                                                        className='Experience-first-address-wrapper'
+                                                        >
+                                                            <div className='Experience-first-imager-inner-wrapper'>
+                                                                <picture>
+                                                                    <source
+                                                                    srcSet='https://a0.muscache.com/im/pictures/a6b08861-feb8-4a01-a76d-b33d2867b441.jpg?im_w=320'
+                                                                    ></source>
+                                                                    <img
+                                                                    className='Experience-image-attr'
+                                                                    src='https://a0.muscache.com/im/pictures/a6b08861-feb8-4a01-a76d-b33d2867b441.jpg?im_w=720'
+                                                                    alt='https://a0.muscache.com/im/pictures/a6b08861-feb8-4a01-a76d-b33d2867b441.jpg?im_w=720'
+                                                                    ></img>
+                                                                </picture>
+                                                            </div>
+                                                            <div className='Experience-first-image-explain-wrapper'>
+                                                                <div className='recomendation-title-word-outter-wrapper'>
+                                                                    <span className='First-image-title-attr'>온라인 체험</span>
+                                                                </div>
+                                                                <div className='recomendation-contents-word-outter-wrapper'>
+                                                                    <span className='First-image-content-attr'>호스트와 실시간으로 소통하면서 액티비티를 즐겨보세요.</span>
+                                                                </div>
+                                                            </div>
+                                                        </a>
+                                                    </div>
+                                                </li>
+                                                <li 
+                                                aria-hidden='false'
+                                                className='Experience-third-image-total-wrapper'
+                                                >
+                                                    <div className='Experience-first-image-inner-wrapper'>
+                                                        <a
+                                                        href='/'
+                                                        className='Experience-first-address-wrapper'
+                                                        >
+                                                            <div className='Experience-first-imager-inner-wrapper'>
+                                                                <picture>
+                                                                    <source
+                                                                    srcSet='https://a0.muscache.com/im/pictures/ad109d56-2421-40cd-98e6-e114160dc85b.jpg?im_w=320'
+                                                                    ></source>
+                                                                    <img
+                                                                    className='Experience-image-attr'
+                                                                    src='https://a0.muscache.com/im/pictures/ad109d56-2421-40cd-98e6-e114160dc85b.jpg?im_w=720'
+                                                                    alt='https://a0.muscache.com/im/pictures/ad109d56-2421-40cd-98e6-e114160dc85b.jpg?im_w=720'
+                                                                    ></img>
+                                                                </picture>
+                                                            </div>
+                                                            <div className='Experience-first-image-explain-wrapper'>
+                                                                <div className='recomendation-title-word-outter-wrapper'>
+                                                                    <span className='First-image-title-attr'>체험</span>
+                                                                </div>
+                                                                <div className='recomendation-contents-word-outter-wrapper'>
+                                                                    <span className='First-image-content-attr'>어디에서든 세계 각지의 매력을 만나실 수 있습니다.</span>
+                                                                </div>
+                                                            </div>
+                                                        </a>
+                                                    </div>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
